9fcb71524ae994d41668a3bc6625d37e862c36e2
Piper McCorkle <contact@piperswe.me>
2021-11-11 17:32:47 -0600

    Use mkRustFlake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs;
-    flake-utils.url = github:numtide/flake-utils;
-    rust-overlay.url = github:oxalica/rust-overlay;
+    mkRustFlake.url = github:piperswe/mkrustflake;
 
     flake-compat = {
       url = github:edolstra/flake-compat;
@@ -10,35 +8,10 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          rust-overlay.overlay
-        ];
-      };
-      rust = pkgs.rust-bin.stable.latest.default.override {
-        extensions = [ "rust-src" "clippy" ];
-      };
-    in
-    {
-      packages.nix_evaluator = pkgs.rustPlatform.buildRustPackage {
-        pname = "nix_evaluator";
-        version = "0.0.0";
-        nativeBuildInputs = [
-          rust
-        ];
-
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-      };
-      defaultPackage = self.packages.${system}.nix_evaluator;
-
-      devShell = self.defaultPackage.${system};
-
-      hydraJobs = {
-        packages = self.packages.${system};
-      };
-    });
+  outputs = { self, mkRustFlake, ... }: mkRustFlake.lib.mkRustFlake {
+    name = "nix_evaluator";
+    version = "0.0.0";
+    src = ./.;
+    cargoLock = ./Cargo.lock;
+  };
 }
